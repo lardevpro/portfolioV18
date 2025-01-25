b0557624dc992eddb7c4966e1fc3511049097873
lardevpro <lardevpro@gmail.com>
2025-01-25 05:51:13 +0100

    project street figther

--- src/app/components/home/projects/projects.component.ts
+++ src/app/components/home/projects/projects.component.ts
@@ -28,10 +28,33 @@ export class ProjectsComponent {
       websiteUrl: 'https://api-angular-spring.web.app',
       imageUrl: 'assets/images/api_rest_img.jpeg',
       gitHubUrl: 'https://github.com/lardevpro/srping-angular',
+      downloadUrl: '',
+    },
+    {
+      title: 'Street Figther',
+      subtitle: 'Java SE',
+      description: `
+        Desarrollé una aplicación que conecta un frontend Angular
+        con una API REST en Spring Boot mediante peticiones HTTP.
+        Gestiona datos temporalmente sin usar base de datos.
+        El proyecto demuestra la interacción eficiente entre frontend y backend.
+      `,
+      repoUrl: 'https://github.com/lardevpro/spring-angular',
+      websiteUrl: '',
+      imageUrl: 'assets/images/street_fighter_prject.jpg',
+      gitHubUrl: 'https://github.com/lardevpro/Street-Figther',
+      downloadUrl: 'https://drive.google.com/file/d/12gwWbLQ4N5BiV2zIijxaZbUACyE3Z6Qw/view?usp=sharing',
     }
   ];
 
   navigateToSite(url: string): void {
     window.open(url, '_blank');
+  }
+
+  downloadFile(filePath: string, fileName: string): void {
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = fileName;
+    link.click();
   }
 }

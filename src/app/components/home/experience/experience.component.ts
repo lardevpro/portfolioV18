0428d957ea2aefb94168e51824a9365050b894e6
lardevpro <lardevpro@gmail.com>
2025-01-26 19:00:02 +0100

    create experience acordeon

--- src/app/components/home/experience/experience.component.ts
+++ src/app/components/home/experience/experience.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Experience } from '../models/experience';
 import { CommonModule } from '@angular/common';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-experience',
-  imports: [CommonModule],
+  imports: [CommonModule,
+            AccordionModule
+            ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
   @Input() experiences!:Experience[];
+  openedPanels: number[] = [0];
 
   splitDescription(description: string): string[] {
     return description
--- src/app/components/home/services/experience.service.ts
+++ src/app/components/home/services/experience.service.ts
@@ -15,7 +15,7 @@ export class ExperienceService  {
       company: 'Accuro Technology',
       location: 'Las Rozas de Madrid (Madrid)',
       logo: 'assets/images/accuro.png',
-      date: 'sep 2024 - actualidad',
+      date: 'sep. 2024 -dic. 2024',
       position: 'Asistente de Desarrollo de Software y Sistemas',
       description: `Apoyo en el desarrollo frontend de aplicaciones web 
       usando Angular.Colaboración en la implementación y personalización de sistemas ERP.
@@ -24,23 +24,51 @@ export class ExperienceService  {
       technologies: ['Angular','Odoo','WordPress', 'Python','HTML', 'CSS', 
                     'JavaScript', 'TypeScript'],
     },{
-      company: 'Kortimed',
+      company: 'Logintia - Logística y Transporte',
+      logo: 'assets/images/kortimed.png',
+      position: 'Conductor Profeisonal (internacional)',
+      location: 'Córdoba (Andalucía, España)',
+      date: 'may. 2024 - ago. 2024',
+      description: `Revisión y mantenimiento de flota de vehículos.
+                    Gestion administrativa de documentación de transporte.
+                    Gestión del Tiempo y Planificación de Rutas.
+                    `,
+      technologies: ['Transporte', 'Logística', 
+      'Conducción', 'Seguridad', 'Mantenimiento','Responsabilidad'],	
+    },
+    {
+      company: 'Redur',
       logo: 'assets/images/kortimed.png',
       position: 'Conductor Profeisonal',
-      location: 'Riba Roja de Túria (Valencia)',
-      date: '2015 - 2018',
-      description: `Transporte de productos alimenticios en cisternas acondicionadas de gran 
-      tonelaje.`,
+      location: 'Madrid (Madird)',
+      date: 'jun. 2023 - ago. 2023',
+      description: `Transporte de mercancías por carretera.
+                    Gestión del Tiempo y Planificación de Rutas.
+                    Revisión y mantenimiento de flota de vehículos.`,
       technologies: ['Transporte', 'Logística', 
       'Conducción', 'Seguridad', 'Mantenimiento','Responsabilidad'],	
     },
   {
-    company: 'Repsol SA',
+    company: 'Taisa Logistcs',
     logo: 'assets/images/repsol.png',
-    position:'Encargado de Estación de Servicios',
-    location: 'Ciudad Real (Ciudad Real)',
-    date: '2015 - 2018',
-    description: `Encargado de tienda y repostaje de cobustibles en estación de servicio Repsol.`,
+    position:'Conductor Profesional',
+    location: 'Madrid (Madrid)',
+    date: 'feb. 2020 - nov. 2020',
+    description: `Conducción segura y cumplimiento de las normas de circulación.
+                  Mantenimiento de vehiculos.
+                  Gestión de documentación de transporte.
+                  `,
+    technologies: ['Producción', 'Automoción', 'Trabajo en equipo', 'Responsabilidad'],
+  },
+  {
+    company: 'Kortimed SRL',
+    logo: 'assets/images/repsol.png',
+    position:'Conductor Profesional (internacional)',
+    location: 'Ribarroja de Turia (Valencia)',
+    date: 'abr. 2017 - dic. 2019',
+    description: `Gestión de documentación.
+                  Encargado de mecánica y mantenimiento de vehiculos.
+                  Conducción segura y cumplimiento de las normas de circulación.`,
     technologies: ['Producción', 'Automoción', 'Trabajo en equipo', 'Responsabilidad'],
   }];
     return of(this.experience);
